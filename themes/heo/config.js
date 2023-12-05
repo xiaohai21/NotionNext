ab96709d3d3dcc87eb1722fd409b508ac5121166
xiaohai21 <114091575+xiaohai21@users.noreply.github.com>
2023-12-05 17:02:36 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -5,8 +5,8 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: '欢迎来到我的博客', url: '#' },
+    { title: '获取更多帮助加博主个人微信13349421745，添加备注来意', url: '#' }
   ],
 
   // 英雄区(首页顶部大卡)
